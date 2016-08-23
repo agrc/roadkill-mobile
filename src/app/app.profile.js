a0dee5bc3e5826d87a89d881e2e440cfb3842b99
Scott Davis <stdavis@utah.gov>
2016-08-23 14:28:54 -0600

    Web mercator (#6)
    * remove duplicate files with ones in desktop project
    
    * tab -> spaces
    
    * remove old error logger
    
    * implement grunt
    
    * processhtml and other build improvements
    
    * footer layout tweaks
    
    * remove old tests
    
    They didn’t work anyway and jquerymobile does not play nice with
    jasmine.
    
    * optimize images
    
    * build system upgrade

---
+++ src/app/app.profile.js
@@ -0,0 +1,21 @@
+/*eslint-disable no-unused-vars, no-undef */
+profile = {
+    resourceTags: {
+        test: function (mid) {
+            return /\/Spec/.test(mid);
+        },
+        copyOnly: function (filename, mid) {
+            return (/^app\/resources\//.test(mid) && !/\.css$/.test(filename));
+        },
+        amd: function (filename, mid) {
+            return !this.copyOnly(filename, mid) && /\.js$/.test(filename);
+        },
+        miniExclude: function (filename, mid) {
+            return mid in {
+                'app/package': 1,
+                'app/tests/jasmineTestBootstrap': 1
+            };
+        }
+    }
+};
+/*eslint-enable no-unused-vars, no-undef */
