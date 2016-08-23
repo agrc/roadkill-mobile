b2653974d2cf20a1783e0177076ae1f9fabf5f17
Scott Davis <stdavis@utah.gov>
2016-08-23 14:28:54 -0600

    Web mercator (#6)
    * remove duplicate files with ones in desktop project
    
    * tab -> spaces
    
    * remove old error logger
    
    * implement grunt
    
    * processhtml and other build improvements
    
    * footer layout tweaks
    
    * remove old tests
    
    They didn’t work anyway and jquerymobile does not play nice with
    jasmine.
    
    * optimize images
    
    * build system upgrade

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # roadkill-mobile
 Wildlife Vehicle Collision Mobile App
+
+## Deployment (these services support both mobile and desktop)
+1. Publish `maps/Overlays.mxd` as `Roadkill/Overlays` (open).
+1. Publish `maps/MapService.mxd` as `Roadkill/MapService` (secure).
+    - Enable feature access with "Create" and "Query" allowed operations.
+    - Max records returned: 10000
+1. Publish `maps/FeatureService.mxd` as `Roadkill/FeatureService` (secure).
+    - Enable feature access with "Create", "Query", "Update", & "Delete" allowed operations.
+1. Publish `scripts\PublicToolbox\SendDiagnostics` as `Roadkill/PublicToolbox/SendDiagnostics` (open).
+    - You may need to manually copy the `agrc` package to the `...\arcgisserver\directories\arcgissystem\arcgisinput\Roadkill\PublicToolbox.GPServer\extracted\v101\scripts` folder.
+1. Publish `roadkill-desktop\scripts\Toolbox\*` as `Roadkill/Toolbox/*` (secure).
+    - `RouteMilepostsSegment`: Params: "0015", "100", "110"
+    - `DownloadData`: Params: "1 = 2", "shp"
+        - You may need to manually copy the `*.sde` file associated with this tool to the `C:\arcgisserver\directories\arcgissystem\arcgisinput\Roadkill\Toolbox.GPServer\extracted\v101\scripts\` directory.
+    - `Print`: Params:
+        - `baseMap`: "Terrain"
+        - `extent`: `{"xmin": 1, "xmax": 2, "ymin": 1, "ymax": 2}`
---
+++ profiles/build.profile.js
@@ -0,0 +1,64 @@
+/*eslint-disable no-unused-vars*/
+var profile = {
+    basePath: '../src',
+    action: 'release',
+    cssOptimize: 'comments',
+    mini: true,
+    optimize: 'uglify',
+    layerOptimize: 'uglify',
+    stripConsole: 'all',
+    selectorEngine: 'acme',
+    layers: {
+        'dojo/dojo': {
+            include: [
+                'dojo/i18n',
+                'dojo/domReady',
+                'app/packages',
+                'app/run',
+                'app/App',
+                'jquery-mobile-bower'
+            ],
+            includeLocales: ['en-us'],
+            customBase: true,
+            boot: true
+        }
+    },
+    packages: ['dijit', {
+        name: 'proj4',
+        resourceTags: {
+            amd: function () {
+                return true;
+            },
+            miniExclude: function (filename, mid) {
+                return mid !== 'proj4/proj4-src';
+            }
+        },
+    }, {
+        name: 'jquery',
+        resourceTags: {
+            copyOnly: function (filename, mid) {
+                return mid !== 'jquery/jquery';
+            }
+        }
+    }],
+    staticHasFeatures: {
+        // The trace & log APIs are used for debugging the loader, so we don’t need them in the build
+        'dojo-trace-api': 0,
+        'dojo-log-api': 0,
+
+        // This causes normally private loader data to be exposed for debugging, so we don’t need that either
+        'dojo-publish-privates': 0,
+
+        // We’re fully async, so get rid of the legacy loader
+        'dojo-sync-loader': 0,
+
+        // dojo-xhr-factory relies on dojo-sync-loader
+        'dojo-xhr-factory': 0,
+
+        // We aren’t loading tests in production
+        'dojo-test-sniff': 0
+    },
+    userConfig: {
+        packages: ['app']
+    }
+};
