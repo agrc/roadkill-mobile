819b1e6aa1ee2e54a3dc5e20e3dfc7ffb8eb9054
Scott Davis <stdavis@utah.gov>
2016-08-23 14:28:54 -0600

    Web mercator (#6)
    * remove duplicate files with ones in desktop project
    
    * tab -> spaces
    
    * remove old error logger
    
    * implement grunt
    
    * processhtml and other build improvements
    
    * footer layout tweaks
    
    * remove old tests
    
    They didn’t work anyway and jquerymobile does not play nice with
    jasmine.
    
    * optimize images
    
    * build system upgrade

---
+++ src/app/data/species.js
@@ -0,0 +1,116 @@
+define([], function () {
+    return [
+        {
+            'name': 'Badger',
+            'code': 'Badger'
+        }, {
+            'name': 'Beaver',
+            'code': 'Beaver'
+        }, {
+            'name': 'Bison',
+            'code': 'Bison'
+        }, {
+            'name': 'Bear',
+            'code': 'Bear'
+        }, {
+            'name': 'Black-footed Ferret',
+            'code': 'Black-footed Ferret'
+        }, {
+            'name': 'Bobcat',
+            'code': 'Bobcat'
+        }, {
+            'name': 'Cougar',
+            'code': 'Cougar'
+        }, {
+            'name': 'Coyote',
+            'code': 'Coyote'
+        }, {
+            'name': 'Desert Bighorn',
+            'code': 'Desert Bighorn'
+        }, {
+            'name': 'Domestic Cat',
+            'code': 'Domestic Cat'
+        }, {
+            'name': 'Domestic Cow',
+            'code': 'Domestic Cow'
+        }, {
+            'name': 'Domestic Dog',
+            'code': 'Domestic Dog'
+        }, {
+            'name': 'Elk',
+            'code': 'Elk'
+        }, {
+            'name': 'Gray Fox',
+            'code': 'Gray Fox'
+        }, {
+            'name': 'Kit Fox',
+            'code': 'Kit Fox'
+        }, {
+            'name': 'Marten',
+            'code': 'Marten'
+        }, {
+            'name': 'Mink',
+            'code': 'Mink'
+        }, {
+            'name': 'Moose',
+            'code': 'Moose'
+        }, {
+            'name': 'Mule Deer',
+            'code': 'Mule Deer'
+        }, {
+            'name': 'Muskrat',
+            'code': 'Muskrat'
+        }, {
+            'name': 'Porcupine',
+            'code': 'Porcupine'
+        }, {
+            'name': 'Prairie Dog',
+            'code': 'Prairie Dog'
+        }, {
+            'name': 'Pronghorn',
+            'code': 'Pronghorn'
+        }, {
+            'name': 'Rabbit/Hare',
+            'code': 'Rabbit/Hare'
+        }, {
+            'name': 'Raccoon',
+            'code': 'Raccoon'
+        }, {
+            'name': 'Red Fox',
+            'code': 'Red Fox'
+        }, {
+            'name': 'Ringtail',
+            'code': 'Ringtail'
+        }, {
+            'name': 'River Otter',
+            'code': 'River Otter'
+        }, {
+            'name': 'Rocky Mountain Bighorn',
+            'code': 'Rocky Mountain Bighorn'
+        }, {
+            'name': 'Rocky Mountain Goat',
+            'code': 'Rocky Mountain Goat'
+        }, {
+            'name': 'Spotted Skunk',
+            'code': 'Spotted Skunk'
+        }, {
+            'name': 'Striped Skunk',
+            'code': 'Striped Skunk'
+        }, {
+            'name': 'Unidentifiable',
+            'code': 'Unidentifiable'
+        }, {
+            'name': 'Unknown',
+            'code': 'Unknown'
+        }, {
+            'name': 'Weasel',
+            'code': 'Weasel'
+        }, {
+            'name': 'Wolf',
+            'code': 'Wolf'
+        }, {
+            'name': 'Wolverine',
+            'code': 'Wolverine'
+        }
+    ];
+});
