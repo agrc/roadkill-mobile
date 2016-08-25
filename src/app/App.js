11a5ec37dd95805490b09b53d19993f03f5ec054
Scott Davis <stdavis@utah.gov>
2016-08-25 14:00:39 -0600

    Release v2.0.0

--- src/app/App.js
+++ src/app/App.js
@@ -34,7 +34,7 @@ define([
         //      The main object that controls the application
 
         // version: String
-        version: '1.3.0',
+        version: '2.0.0',
 
         // gpsController: GPSController
         gpsController: null,
