a73026431a406378e7f1fbde90fd217a1f1bb831
steveoh <sgourley@utah.gov>
2019-03-14 14:37:24 -0600

    chore: remove appcache

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,7 @@ module.exports = function (grunt) {
                         }
                     }]
                 },
-                files: {'dist/roadkill.appcache': ['src/roadkill.appcache']}
+                files: {'dist/serviceworker.js': ['src/serviceworker.js']}
             }
         },
         secrets: secrets,
