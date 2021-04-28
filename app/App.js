c4af09b585c1fcc73e26cd4e6add3020167363a9
stdavis <stdavis@utah.gov>
2021-04-28 11:18:48 -0600

    feat(front): simple poc auth app

--- app/App.js
+++ app/App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import useAuth from './auth';
+
+if(__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 
 export default function App() {
+  const {userInfo, getAccessToken, logIn, logOut, status} = useAuth();
+  const [token, setToken] = React.useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        disabled={status === 'pending'}
+        title="Login"
+        onPress={() => {
+          logIn()
+        }}
+      />
+      {<Text>user email: {userInfo?.email}</Text>}
+      {<Text>bearer token: {token?.slice(0, 10)}</Text>}
+      {<Text>status: {status}</Text>}
+      <Button
+        title="Get Access Token"
+        onPress={() => {
+          getAccessToken().then(accessToken => setToken(accessToken)).catch(error => console.error(error));
+        }}
+      />
+      <Button
+        title="Log Out"
+        onPress={() => {
+          logOut();
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    display: 'flex',
+    height: '100%'
   },
 });
