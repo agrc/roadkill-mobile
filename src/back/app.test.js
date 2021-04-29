b478dfda1255fbfd8357b6c3bd87312af0ce07c8
stdavis <stdavis@utah.gov>
2021-04-29 17:08:28 -0600

    fix(back): separate validation for auth and refresh token requests

--- src/back/app.test.js
+++ src/back/app.test.js
@@ -10,19 +10,45 @@ describe('/token', () => {
       .expect(400)
       .expect(/invalid client_id/, done);
   });
-  it('requires all params', (done) => {
-    // missing grant_type
+  it('requires grant_type always', (done) => {
     const params = {
       client_id: process.env.CLIENT_ID,
       code_verifier: 'blah',
       code_challenge: 'blah',
       redirect_uri: 'blah',
       code: 'code',
+      refresh_token: 'blah',
     };
     request(app)
       .post('/token')
       .send(formurlencoded(params))
       .expect(400)
-      .expect(/grant_type is required/, done);
+      .expect(/invalid grant_type/, done);
+  });
+  it('requires appropriate params when exchanging code for token', (done) => {
+    // missing grant_type
+    const params = {
+      grant_type: 'authorization_code',
+      client_id: process.env.CLIENT_ID,
+      code_verifier: 'blah',
+      code_challenge: 'blah',
+      code: 'code',
+    };
+    request(app)
+      .post('/token')
+      .send(formurlencoded(params))
+      .expect(400)
+      .expect(/redirect_uri is required/, done);
+  });
+  it('requires appropriate params for refresh token', (done) => {
+    const params = {
+      grant_type: 'refresh_token',
+      client_id: process.env.CLIENT_ID,
+    };
+    request(app)
+      .post('/token')
+      .send(formurlencoded(params))
+      .expect(400)
+      .expect(/refresh_token is required/, done);
   });
 });
