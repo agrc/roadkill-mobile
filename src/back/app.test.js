58919d9e7d778cbe3cd4cc840511f8d393398b82
stdavis <stdavis@utah.gov>
2021-04-29 11:37:16 -0600

    fix(back): better token params validation tests

--- src/back/app.test.js
+++ src/back/app.test.js
@@ -1,8 +1,28 @@
 import request from 'supertest';
 import app from './app';
+import formurlencoded from 'form-urlencoded';
 
 describe('/token', () => {
-  it('requires specific parameters', (done) => {
-    request(app).post('/token', { client_id: 'blah' }).expect(400, done);
+  it('requires matching client_id', (done) => {
+    request(app)
+      .post('/token')
+      .send('client_id=blah')
+      .expect(400)
+      .expect(/invalid client_id/, done);
+  });
+  it('requires all params', (done) => {
+    // missing grant_type
+    const params = {
+      client_id: process.env.CLIENT_ID,
+      code_verifier: 'blah',
+      code_challenge: 'blah',
+      redirect_uri: 'blah',
+      code: 'code',
+    };
+    request(app)
+      .post('/token')
+      .send(formurlencoded(params))
+      .expect(400)
+      .expect(/grant_type is required/, done);
   });
 });
