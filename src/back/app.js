04979e24da0a39e06d40dea4408f4f621480ad7b
stdavis <stdavis@utah.gov>
2021-05-06 13:30:28 -0600

    feat(back): add cors support

--- src/back/app.js
+++ src/back/app.js
@@ -2,6 +2,7 @@ import compression from 'compression';
 import express from 'express';
 import helmet from 'helmet';
 import { authenticate, getToken } from './security.js';
+import cors from 'cors';
 
 // app setup
 const app = express();
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // security best practices
 app.use(helmet());
+
+app.use(cors());
 
 function handleAsyncErrors(callback) {
   // this will be done by default at express 5.0
