918763379c738ea82b50f37b53d02dee384a8219
stdavis <stdavis@utah.gov>
2021-05-17 17:04:22 -0600

    chore(front): develop steps for deploying new app builds

---
+++ src/front/deployNewAppBuild.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $1 ]; then
+  RELEASE_CHANNEL=$1
+elif [ -z "$GIT_BRANCH" ]; then
+  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) || exit;
+  RELEASE_CHANNEL=${GIT_BRANCH#"origin/"}
+fi
+
+ENV_FILE="./.env.$RELEASE_CHANNEL"
+echo "getting environment variables from $ENV_FILE"
+set -o allexport
+source $ENV_FILE
+set +o allexport
+
+echo "publishing to $RELEASE_CHANNEL"
+expo publish --release-channel $RELEASE_CHANNEL
+
+echo "building ios and android apps concurrently"
+expo build:ios --release-channel $RELEASE_CHANNEL --no-publish -t archive &
+expo build:android --release-channel $RELEASE_CHANNEL --no-publish -t app-bundle &
+wait
+
+echo "downloading app packages"
+curl -O "$(expo url:ipa)"
+curl -O "$(expo url:apk)"
+
+echo "uploading to testflight"
+fastlane pilot upload -u stdavis@utah.gov
+
+echo "*** don't forget to submit a ticket to DTS to upload the android package"
