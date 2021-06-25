ad8b4232135269ac131f96cd585f37ecbdfbd4fc
stdavis <stdavis@utah.gov>
2021-06-25 10:31:46 -0600

    feat: add screen views to analytics
    Finishes up #56

--- src/front/App.js
+++ src/front/App.js
@@ -5,43 +5,12 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { default as theme } from './custom-theme.json';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import AppNavigator from './AppNavigator';
-import * as Analytics from 'expo-firebase-analytics';
-import * as SecureStorage from 'expo-secure-store';
-import { v4 as uuid } from 'uuid';
-import Constants from 'expo-constants';
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 
 export default function App() {
-  React.useEffect(() => {
-    const initAnalytics = async () => {
-      if (Constants.appOwnership === 'expo') {
-        const key = 'CLIENT_ID';
-        let id = await SecureStorage.getItemAsync(key);
-
-        if (!id) {
-          id = uuid();
-
-          SecureStorage.setItemAsync(key, id);
-        }
-
-        // needs to be called before any other analytics methods
-        Analytics.setClientId(id);
-      }
-
-      if (__DEV__) {
-        Analytics.setDebugModeEnabled(true);
-      }
-
-      Analytics.setCurrentScreen('home'); // doesn't work in expo go
-      Analytics.logEvent('test_event');
-    };
-
-    initAnalytics();
-  }, []);
-
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
