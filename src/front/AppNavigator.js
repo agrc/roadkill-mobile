ede014b6b0b976a4649ff1a9f41ec9882aa606e2
stdavis <stdavis@utah.gov>
2021-06-25 10:31:46 -0600

    feat: add screen views to analytics
    Finishes up #56

--- src/front/AppNavigator.js
+++ src/front/AppNavigator.js
@@ -5,6 +5,10 @@ import ChoseRoleScreen from './screens/ChoseRole';
 import LoginScreen from './screens/Login';
 import MainScreen from './screens/Main';
 import * as Linking from 'expo-linking';
+import * as Analytics from 'expo-firebase-analytics';
+import * as SecureStorage from 'expo-secure-store';
+import { v4 as uuid } from 'uuid';
+import Constants from 'expo-constants';
 
 const { Navigator, Screen } = createStackNavigator();
 const prefix = Linking.createURL('/');
@@ -18,10 +22,53 @@ const HomeNavigator = () => (
 );
 
 const AppNavigator = () => {
+  const navigationRef = React.useRef(null);
+  const routeNameRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const initAnalytics = async () => {
+      if (Constants.appOwnership === 'expo') {
+        const key = 'CLIENT_ID';
+        let id = await SecureStorage.getItemAsync(key);
+
+        if (!id) {
+          id = uuid();
+
+          SecureStorage.setItemAsync(key, id);
+        }
+
+        // needs to be called before any other analytics methods
+        Analytics.setClientId(id);
+      }
+
+      if (__DEV__) {
+        Analytics.setDebugModeEnabled(true);
+      }
+    };
+
+    initAnalytics();
+  }, []);
+
   const linking = { prefixes: [prefix] };
+  const setInitialRouteName = () => (routeNameRef.current = navigationRef.current.getCurrentRoute().name);
+  const updateRouteName = async () => {
+    const previousRouteName = routeNameRef.current;
+    const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+    if (previousRouteName !== currentRouteName) {
+      await Analytics.setCurrentScreen(currentRouteName);
+    }
+
+    routeNameRef.current = currentRouteName;
+  };
 
   return (
-    <NavigationContainer linking={linking}>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={setInitialRouteName}
+      onStateChange={updateRouteName}
+      linking={linking}
+    >
       <HomeNavigator />
     </NavigationContainer>
   );
