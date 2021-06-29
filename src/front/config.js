0861a73740445763eafd018933bca0b9acf8cdb9
stdavis <stdavis@utah.gov>
2021-06-29 12:06:02 -0600

    fix: enable hitting local api from physical device

--- src/front/config.js
+++ src/front/config.js
@@ -1,11 +1,15 @@
-import { Platform } from 'react-native';
 import Constants from 'expo-constants';
 
 let API = process.env.API;
 // localhost/10.0.2.2 only work for emulators running on the same machine as the dev server
 // how to handle separate devices? ngrok? localtunnel? deploy to gcp dev?
-if (__DEV__ && Platform.OS === 'android') {
-  API = API.replace('localhost', '10.0.2.2');
+if (__DEV__) {
+  if (!Constants.isDevice && Constants.platform.android) {
+    API = API.replace('localhost', '10.0.2.2');
+  } else if (Constants.isDevice) {
+    API = API.replace('localhost', Constants.manifest.debuggerHost.split(':').shift());
+    console.log('API', API);
+  }
 }
 
 export default {
