f6a256854ef69158f704fc481f555068742ee009
stdavis <stdavis@utah.gov>
2021-06-29 12:05:44 -0600

    feat: better auth flow
    show splash while loading logged in user
    use context to provider user info

--- src/front/App.js
+++ src/front/App.js
@@ -5,17 +5,43 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { default as theme } from './custom-theme.json';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import AppNavigator from './AppNavigator';
+import { AuthContextProvider } from './auth';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 
 export default function App() {
+  const [authIsReady, setAuthIsReady] = React.useState(false);
+  const splashIsHidden = React.useRef(false);
+
+  React.useEffect(() => {
+    if (!splashIsHidden.current) {
+      SplashScreen.preventAutoHideAsync();
+    }
+  }, []);
+
+  const onReady = () => {
+    if (authIsReady && !splashIsHidden.current) {
+      console.log('hide splash');
+      SplashScreen.hideAsync();
+      splashIsHidden.current = true;
+    }
+  };
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-        <AppNavigator />
+        <AuthContextProvider setReady={setAuthIsReady}>
+          {authIsReady ? (
+            <View onLayout={onReady} style={{ flex: 1 }}>
+              <AppNavigator />
+            </View>
+          ) : null}
+        </AuthContextProvider>
       </ApplicationProvider>
     </>
   );
--- src/front/screens/Main.js
+++ src/front/screens/Main.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Divider, Layout, Text, TopNavigation } from '@ui-kitten/components';
-import { SafeAreaView } from 'react-native';
+import { Button, Divider, Layout, Text, TopNavigation } from '@ui-kitten/components';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import propTypes from 'prop-types';
+import useAuth from '../auth';
 
-export default function MainScreen() {
+export default function MainScreen({ navigation }) {
+  const { logOut } = useAuth(navigation);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title="WVC" alignment="center" />
       <Divider />
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Map</Text>
+        <Button onPress={logOut}>log off</Button>
       </Layout>
     </SafeAreaView>
   );
