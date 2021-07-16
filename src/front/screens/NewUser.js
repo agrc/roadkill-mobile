673b6453fd9b781f78bd7d82210ecdfcb86e7a86
stdavis <stdavis@utah.gov>
2021-07-16 10:06:45 -0600

    feat: add form validation to new user screen

--- src/front/screens/NewUser.js
+++ src/front/screens/NewUser.js
@@ -1,19 +1,30 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, Input, Text } from '@ui-kitten/components';
+import { Formik } from 'formik';
 import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
+import * as yup from 'yup';
+import 'yup-phone';
 import useAuth from '../auth/context';
 import config from '../config';
 import RootView from '../RootView';
 
 export default function NewUserScreen() {
-  const [organization, setOrganization] = React.useState(null);
-  const [phone, setPhone] = React.useState(null);
   const { logOut, userType } = useAuth();
   const navigation = useNavigation();
+  const organizationIsRequired = userType !== config.USER_TYPES.public;
 
-  const register = () => {
-    // TODO
+  const shape = {
+    phone: yup.string().phone('US').required('phone is required'),
+  };
+  if (organizationIsRequired) {
+    shape.organization = yup.string().required('organization is required');
+  }
+  const schema = yup.object().shape(shape);
+
+  const register = (values, { setSubmitting }) => {
+    console.log('values', values);
+    setSubmitting(false);
   };
   const cancel = () => {
     navigation.navigate('login');
@@ -26,30 +37,42 @@ export default function NewUserScreen() {
       <ScrollView style={styles.layout}>
         <Text category="h1">Please complete your profile</Text>
 
-        <View style={styles.inputsContainer}>
-          <Input style={styles.input} label="Name" value={authInfo?.user.name} disabled />
-          <Input style={styles.input} label="Email" value={authInfo?.user.email} disabled />
-          {userType !== config.USER_TYPES.public ? (
-            <Input
-              style={styles.input}
-              label="Organization"
-              textContentType="organizationName"
-              value={organization}
-              onChangeText={setOrganization}
-            />
-          ) : null}
-          <Input
-            style={styles.input}
-            label="Phone"
-            keyboardType="phone-pad"
-            textContentType="telephoneNumber"
-            value={phone}
-            onChangeText={setPhone}
-          />
-        </View>
-        <Button onPress={register} style={styles.input}>
-          Register
-        </Button>
+        <Formik initialValues={{ phone: null, organization: null }} validationSchema={schema} onSubmit={register}>
+          {({ values, handleChange, handleBlur, handleSubmit, errors, isValid, touched }) => (
+            <>
+              <View style={styles.inputsContainer}>
+                <Input style={styles.input} label="Name" value={authInfo?.user.name} disabled />
+                <Input style={styles.input} label="Email" value={authInfo?.user.email} disabled />
+                {organizationIsRequired ? (
+                  <Input
+                    style={styles.input}
+                    label="Organization"
+                    caption={errors.organization && touched.email ? errors.organization : null}
+                    textContentType="organizationName"
+                    value={values.organization}
+                    onChangeText={handleChange('organization')}
+                    onBlur={handleBlur('organization')}
+                    status={errors.organization ? 'danger' : null}
+                  />
+                ) : null}
+                <Input
+                  style={styles.input}
+                  label="Phone"
+                  caption={errors.phone && touched.phone ? errors.phone : null}
+                  keyboardType="phone-pad"
+                  textContentType="telephoneNumber"
+                  value={values.phone}
+                  onChangeText={handleChange('phone')}
+                  onBlur={handleBlur('phone')}
+                  status={errors.phone ? 'danger' : null}
+                />
+              </View>
+              <Button onPress={handleSubmit} style={styles.input} disabled={!isValid}>
+                Register
+              </Button>
+            </>
+          )}
+        </Formik>
         <Button onPress={cancel} style={styles.input} status="warning" appearance="outline">
           Cancel
         </Button>
