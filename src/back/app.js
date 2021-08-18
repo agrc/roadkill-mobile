fe76355ea09bd36feef2f4e39e336c5ea5cbd3ee
stdavis <stdavis@utah.gov>
2021-08-18 12:07:38 -0600

    fix(back): don't log to gcp in test or development

--- src/back/app.js
+++ src/back/app.js
@@ -36,48 +36,50 @@ app.use(helmet());
 
 app.use(cors());
 
-app.use(
-  expressWinston.logger({
-    transports: [new LoggingWinston()],
-    metaField: null, //this causes the metadata to be stored at the root of the log entry
-    responseField: null, // this prevents the response from being included in the metadata (including body and status code)
-    requestWhitelist: ['url', 'method', 'httpVersion', 'originalUrl', 'query', 'body'],
-    responseWhitelist: ['body'], // this populates the `res.body` so we can get the response size (not required)
-    format: winston.format.json(),
-    ignoreRoute: (request) => request?.route?.path === '/user/login',
-    dynamicMeta: (req, res) => {
-      const httpRequest = {};
-      const meta = {};
-      if (req) {
-        meta.httpRequest = httpRequest;
-        httpRequest.requestMethod = req.method;
-        httpRequest.requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-        httpRequest.remoteIp = req.ip.indexOf(':') >= 0 ? req.ip.substring(req.ip.lastIndexOf(':') + 1) : req.ip; // just ipv4
-        httpRequest.requestSize = req.socket.bytesRead;
-        httpRequest.userAgent = req.get('User-Agent');
-        httpRequest.referrer = req.get('Referrer');
-      }
+if (process.env.ENVIRONMENT !== 'development' && process.env.ENVIRONMENT !== 'test') {
+  app.use(
+    expressWinston.logger({
+      transports: [new LoggingWinston()],
+      metaField: null, //this causes the metadata to be stored at the root of the log entry
+      responseField: null, // this prevents the response from being included in the metadata (including body and status code)
+      requestWhitelist: ['url', 'method', 'httpVersion', 'originalUrl', 'query', 'body'],
+      responseWhitelist: ['body'], // this populates the `res.body` so we can get the response size (not required)
+      format: winston.format.json(),
+      ignoreRoute: (request) => request?.route?.path === '/user/login',
+      dynamicMeta: (req, res) => {
+        const httpRequest = {};
+        const meta = {};
+        if (req) {
+          meta.httpRequest = httpRequest;
+          httpRequest.requestMethod = req.method;
+          httpRequest.requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+          httpRequest.remoteIp = req.ip.indexOf(':') >= 0 ? req.ip.substring(req.ip.lastIndexOf(':') + 1) : req.ip; // just ipv4
+          httpRequest.requestSize = req.socket.bytesRead;
+          httpRequest.userAgent = req.get('User-Agent');
+          httpRequest.referrer = req.get('Referrer');
+        }
 
-      if (res) {
-        meta.httpRequest = httpRequest;
-        httpRequest.status = res.statusCode;
-        httpRequest.latency = {
-          seconds: Math.floor(res.responseTime / 1000),
-          nanos: (res.responseTime % 1000) * 1000000,
-        };
-        if (res.body) {
-          if (typeof res.body === 'object') {
-            httpRequest.responseSize = JSON.stringify(res.body).length;
-          } else if (typeof res.body === 'string') {
-            httpRequest.responseSize = res.body.length;
+        if (res) {
+          meta.httpRequest = httpRequest;
+          httpRequest.status = res.statusCode;
+          httpRequest.latency = {
+            seconds: Math.floor(res.responseTime / 1000),
+            nanos: (res.responseTime % 1000) * 1000000,
+          };
+          if (res.body) {
+            if (typeof res.body === 'object') {
+              httpRequest.responseSize = JSON.stringify(res.body).length;
+            } else if (typeof res.body === 'string') {
+              httpRequest.responseSize = res.body.length;
+            }
           }
         }
-      }
-      return meta;
-    },
-    meta: true,
-  })
-);
+        return meta;
+      },
+      meta: true,
+    })
+  );
+}
 
 function handleAsyncErrors(callback) {
   // could be replaced with https://www.npmjs.com/package/express-async-errors
