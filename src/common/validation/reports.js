89a98b22025db8884a72588982739245b4ba61ff
stdavis <stdavis@utah.gov>
2021-10-18 16:53:21 -0600

    feat: add comments component
    Closes #101

--- src/common/validation/reports.js
+++ src/common/validation/reports.js
@@ -15,6 +15,7 @@ const species = yup.string().nullable();
 const species_confidence_level = yup.string().nullable();
 const age_class = yup.string().required();
 const sex = yup.string().required();
+const comments = yup.string().nullable();
 
 export const report = yup.object().shape({
   user_id,
@@ -29,6 +30,7 @@ export const report = yup.object().shape({
   species_confidence_level,
   age_class,
   sex,
+  comments,
 });
 
 export const pickup = yup.object().shape({
@@ -43,4 +45,5 @@ export const pickup = yup.object().shape({
   species_confidence_level,
   age_class,
   sex,
+  comments,
 });
