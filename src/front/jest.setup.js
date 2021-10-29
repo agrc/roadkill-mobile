60cb325e534a01b5669716a9e9de7bfb9e97cfd8
stdavis <stdavis@utah.gov>
2021-10-29 15:57:48 -0600

    chore(front): :everygreen_tree: bump expo sdk to 43
    And bump everything else that I can on the front end.

--- src/front/jest.setup.js
+++ src/front/jest.setup.js
@@ -1,6 +1,6 @@
 // Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
 // Use with React Native <= 0.63
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+// jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 // Use this instead with React Native >= 0.64
-// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
