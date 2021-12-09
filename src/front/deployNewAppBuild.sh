f0e1c2bce04cdc3ac289617278e3b207d8394c87
stdavis <stdavis@utah.gov>
2021-12-09 15:36:28 -0700

    build: fix issues with downloading artifacts from eas

--- src/front/deployNewAppBuild.sh
+++ src/front/deployNewAppBuild.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-RELEASE_CHANNEL=$(./getReleaseChannel.sh)
 echo "Building and deploying new app builds for release channel: $RELEASE_CHANNEL"
 
 RELEASE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -12,13 +11,13 @@ source $ENV_FILE
 set +o allexport
 
 echo "building ios and android apps concurrently"
-eas build --platform all --profile $RELEASE_CHANNEL
+eas build --platform all --profile $RELEASE_BRANCH
 
 # TODO: switch to eas submit if DTS ever grants me access to the necessary app store/play store api's
 echo "downloading artifacts"
-ANDROID_URL = eas build:list --platform android --json --limit 1 | jq '.[0].artifacts.buildUrl'
+ANDROID_URL=$(eas build:list --platform android --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
 curl -LO $ANDROID_URL
-IOS_URL = eas build:list --platform ios --json --limit 1 | jq '.[0].artifacts.buildUrl'
+IOS_URL=$(eas build:list --platform ios --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
 curl -LO $IOS_URL
 
 echo "uploading to testflight"
