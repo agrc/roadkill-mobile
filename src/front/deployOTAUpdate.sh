f2f9ed8787e57cb937b46a8b154f3a6ea55de6c3
stdavis <stdavis@utah.gov>
2021-12-09 15:37:36 -0700

    build: create ota update script

---
+++ src/front/deployOTAUpdate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+RELEASE_CHANNEL=$(./getReleaseChannel.sh)
+echo "Publishing new app builds for release channel: $RELEASE_CHANNEL"
+
+RELEASE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+ENV_FILE="./.env.$RELEASE_BRANCH"
+echo "getting environment variables from $ENV_FILE"
+set -o allexport
+source $ENV_FILE
+set +o allexport
+
+expo publish --release-channel $RELEASE_CHANNEL
