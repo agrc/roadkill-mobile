a23985f16fb32ee883df65da97449042ddf18df1
stdavis <stdavis@utah.gov>
2022-01-25 11:02:22 -0700

    chore: better echos

--- src/front/scripts/downloadArtifacts.sh
+++ src/front/scripts/downloadArtifacts.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "downloading artifacts"
+echo "downloading android..."
 ANDROID_URL=$(eas build:list --platform android --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
 curl -LO $ANDROID_URL
+
+echo "downloading ios..."
 IOS_URL=$(eas build:list --platform ios --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
 curl -LO $IOS_URL
