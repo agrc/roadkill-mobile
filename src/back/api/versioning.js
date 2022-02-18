80fe4185a648f3b1d89314be3688a1fea3f7ee0c
stdavis <stdavis@utah.gov>
2022-02-18 14:52:19 -0700

    chore(back): version api to handle v0.0.0 requests

--- src/back/api/versioning.js
+++ src/back/api/versioning.js
@@ -1,9 +1,20 @@
 import commonConfig from 'common/config.js';
+import lt from 'semver/functions/lt.js';
 
 export default function getVersionFromHeader(request, response, next) {
-  const version = request.get(commonConfig.versionHeaderName);
-  if (version) {
-    request.version = version;
-  }
+  let version = request.get(commonConfig.versionHeaderName) || '0.0.0';
+
+  request.version = version;
+
   next();
+}
+
+export function switchByRequestVersion(minVersion, oldReturn, newReturn) {
+  return (request, ...rest) => {
+    if (lt(request.version, minVersion)) {
+      return oldReturn(request, ...rest);
+    }
+
+    return newReturn(request, ...rest);
+  };
 }
