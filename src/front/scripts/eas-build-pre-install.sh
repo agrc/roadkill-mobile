c624de29c0e117ad710af32a0880d0adab13d294
stdavis <stdavis@utah.gov>
2022-03-08 10:55:43 -0700

    feat(front): create dev and staging apps
    Closes #152

--- src/front/scripts/eas-build-pre-install.sh
+++ src/front/scripts/eas-build-pre-install.sh
@@ -15,20 +15,28 @@ if [[ $EAS_BUILD_PROFILE == "production" ]]; then
   export FIREBASE_MEASUREMENT_ID="$PRODUCTION_FIREBASE_MEASUREMENT_ID"
 fi
 
-if [[ $EAS_BUILD_PROFILE != "production" ]]; then
-  echo "building google services files for non-production"
-
-  echo $STAGING_GOOGLE_SERVICES_ANDROID_BASE64 | base64 -d > ./google-services.json
-  echo $STAGING_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
-  export GOOGLE_MAPS_API_KEY_ANDROID="$STAGING_GOOGLE_MAPS_API_KEY_ANDROID"
-  export GOOGLE_MAPS_API_KEY_IOS="$STAGING_GOOGLE_MAPS_API_KEY_IOS"
-else
+if [[ $EAS_BUILD_PROFILE == "production" ]]; then
   echo "building google services files for production"
 
   echo $PRODUCTION_GOOGLE_SERVICES_ANDROID_BASE64 | base64 -d > ./google-services.json
   echo $PRODUCTION_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
   export GOOGLE_MAPS_API_KEY_ANDROID="$PRODUCTION_GOOGLE_MAPS_API_KEY_ANDROID"
   export GOOGLE_MAPS_API_KEY_IOS="$PRODUCTION_GOOGLE_MAPS_API_KEY_IOS"
+elif [[ $EAS_BUILD_PROFILE == "staging" ]]; then
+  echo "building google services files for staging"
+
+  echo $STAGING_GOOGLE_SERVICES_ANDROID_BASE64 | base64 -d > ./google-services.json
+  echo $STAGING_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
+  export GOOGLE_MAPS_API_KEY_ANDROID="$STAGING_GOOGLE_MAPS_API_KEY_ANDROID"
+  export GOOGLE_MAPS_API_KEY_IOS="$STAGING_GOOGLE_MAPS_API_KEY_IOS"
+else
+  echo "building google services files for development"
+
+  echo $DEV_GOOGLE_SERVICES_ANDROID_BASE64 | base64 -d > ./google-services.json
+  echo $DEV_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
+  #: staging map api keys are restricted to both staging and dev apps...
+  export GOOGLE_MAPS_API_KEY_ANDROID="$STAGING_GOOGLE_MAPS_API_KEY_ANDROID"
+  export GOOGLE_MAPS_API_KEY_IOS="$STAGING_GOOGLE_MAPS_API_KEY_IOS"
 fi
 
 echo "exporting env vars required at runtime to .env file"
