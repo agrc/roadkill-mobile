e21e70441084d2f7bb2898c419abf99c22550e75
stdavis <stdavis@utah.gov>
2022-03-08 14:35:30 -0700

    fix(front): add dev and staging oauth client ids

--- src/front/scripts/eas-build-pre-install.sh
+++ src/front/scripts/eas-build-pre-install.sh
@@ -22,6 +22,8 @@ if [[ $EAS_BUILD_PROFILE == "production" ]]; then
   echo $PRODUCTION_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
   export GOOGLE_MAPS_API_KEY_ANDROID="$PRODUCTION_GOOGLE_MAPS_API_KEY_ANDROID"
   export GOOGLE_MAPS_API_KEY_IOS="$PRODUCTION_GOOGLE_MAPS_API_KEY_IOS"
+  export GOOGLE_OAUTH_CLIENT_ID_ANDROID="$PRODUCTION_GOOGLE_OAUTH_CLIENT_ID_ANDROID"
+  export GOOGLE_OAUTH_CLIENT_ID_IOS="$PRODUCTION_GOOGLE_OAUTH_CLIENT_ID_IOS"
 elif [[ $EAS_BUILD_PROFILE == "staging" ]]; then
   echo "building google services files for staging"
 
@@ -29,6 +31,8 @@ elif [[ $EAS_BUILD_PROFILE == "staging" ]]; then
   echo $STAGING_GOOGLE_SERVICES_IOS_BASE64 | base64 -d > ./GoogleService-Info.plist
   export GOOGLE_MAPS_API_KEY_ANDROID="$STAGING_GOOGLE_MAPS_API_KEY_ANDROID"
   export GOOGLE_MAPS_API_KEY_IOS="$STAGING_GOOGLE_MAPS_API_KEY_IOS"
+  export GOOGLE_OAUTH_CLIENT_ID_ANDROID="$STAGING_GOOGLE_OAUTH_CLIENT_ID_ANDROID"
+  export GOOGLE_OAUTH_CLIENT_ID_IOS="$STAGING_GOOGLE_OAUTH_CLIENT_ID_IOS"
 else
   echo "building google services files for development"
 
@@ -37,6 +41,8 @@ else
   #: staging map api keys are restricted to both staging and dev apps...
   export GOOGLE_MAPS_API_KEY_ANDROID="$STAGING_GOOGLE_MAPS_API_KEY_ANDROID"
   export GOOGLE_MAPS_API_KEY_IOS="$STAGING_GOOGLE_MAPS_API_KEY_IOS"
+  export GOOGLE_OAUTH_CLIENT_ID_ANDROID="$DEV_GOOGLE_OAUTH_CLIENT_ID_ANDROID"
+  export GOOGLE_OAUTH_CLIENT_ID_IOS="$DEV_GOOGLE_OAUTH_CLIENT_ID_IOS"
 fi
 
 echo "exporting env vars required at runtime to .env file"
