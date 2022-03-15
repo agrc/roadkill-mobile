f9f2bbc29c758e6659e69f9b62182fe57321a631
stdavis <stdavis@utah.gov>
2022-03-15 08:52:57 -0600

    build(front): add platform param

--- src/front/scripts/deployNewAppBuild.sh
+++ src/front/scripts/deployNewAppBuild.sh
@@ -8,17 +8,27 @@ set -o allexport
 source $ENV_FILE
 set +o allexport
 
+if [ -z "$1" ]; then
+  PLATFORM="all"
+else
+  PLATFORM="$1"
+fi
+
 echo "Building and deploying new app builds for release channel: $RELEASE_BRANCH"
 
-./scripts/removeArtifacts.sh
+./scripts/removeArtifacts.sh $PLATFORM
 
-echo "building ios and android apps concurrently"
-eas build --platform all --profile $RELEASE_BRANCH --non-interactive
+echo "building $PLATFORM platform(s) concurrently"
+eas build --platform $PLATFORM --profile $RELEASE_BRANCH --non-interactive
 
-./scripts/downloadArtifacts.sh
+./scripts/downloadArtifacts.sh $PLATFORM
 
-# TODO: switch to eas submit if DTS ever grants me access to the necessary app store/play store api's
-echo "uploading to testflight"
-fastlane pilot upload -u stdavis@utah.gov
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "ios" ]; then
+  # TODO: switch to eas submit if DTS ever grants me access to the necessary app store/play store api's
+  echo "uploading to testflight"
+  fastlane pilot upload -u stdavis@utah.gov
+fi
 
-echo "*** don't forget to upload the android package to the Google Play Console at https://play.google.com/console/u/1/developers/6377537875100906890/app/4972434106866476517/bundle-explorer"
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "android" ]; then
+  echo "*** don't forget to upload the android package to the Google Play Console at https://play.google.com/console/u/1/developers/6377537875100906890/app/4972434106866476517/bundle-explorer"
+fi
--- src/front/scripts/removeArtifacts.sh
+++ src/front/scripts/removeArtifacts.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$1" ]; then
+  PLATFORM="all"
+else
+  PLATFORM="$1"
+fi
+
 echo "cleaning up old artifacts"
-rm -f -- *.gz
-rm -f -- *.apk
-rm -f -- *.aab
-rm -f -- *.ipa
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "ios" ]; then
+  rm -f -- *.gz
+  rm -f -- *.ipa
+fi
+
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "android" ]; then
+  rm -f -- *.apk
+  rm -f -- *.aab
+fi
