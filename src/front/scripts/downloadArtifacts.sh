b8b1590b865fa29be7d7bd3df63261fb26c2a295
stdavis <stdavis@utah.gov>
2022-03-15 08:52:57 -0600

    build(front): add platform param

--- src/front/scripts/downloadArtifacts.sh
+++ src/front/scripts/downloadArtifacts.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-echo "downloading android..."
-ANDROID_URL=$(eas build:list --platform android --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
-curl -LO $ANDROID_URL
+if [ -z "$1" ]; then
+  PLATFORM="all"
+else
+  PLATFORM="$1"
+fi
 
-echo "downloading ios..."
-IOS_URL=$(eas build:list --platform ios --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
-curl -LO $IOS_URL
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "android" ]; then
+  echo "downloading android..."
+  ANDROID_URL=$(eas build:list --platform android --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
+  curl -LO $ANDROID_URL
+fi
+
+if [ "$PLATFORM" == "all" or "$PLATFORM" == "ios" ]; then
+  echo "downloading ios..."
+  IOS_URL=$(eas build:list --platform ios --json --limit 1 | jq -r '.[0].artifacts.buildUrl')
+  curl -LO $IOS_URL
+fi
