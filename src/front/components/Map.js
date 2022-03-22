d058d827b00154e2e844cecf4f6e14fae044ccba
stdavis <stdavis@utah.gov>
2022-03-22 17:01:19 -0600

    feat(front): cache visited map tiles for offline use
    This is a start on #29

--- src/front/components/Map.js
+++ src/front/components/Map.js
@@ -1,8 +1,18 @@
+import * as FileSystem from 'expo-file-system';
 import propTypes from 'prop-types';
 import React from 'react';
 import { Platform } from 'react-native';
 import MapView, { MAP_TYPES, UrlTile } from 'react-native-maps';
 import config from '../services/config';
+
+const tileCacheDirectory = `${FileSystem.cacheDirectory}tiles`;
+FileSystem.getInfoAsync(tileCacheDirectory, { size: true }).then((info) => {
+  console.log('tileCacheDirectory info', info);
+  if (!info.exists) {
+    console.log('creating tileCacheDirectory');
+    FileSystem.makeDirectoryAsync(tileCacheDirectory);
+  }
+});
 
 export default function Map({ innerRef, children, isStatic, ...mapViewProps }) {
   if (isStatic) {
@@ -32,6 +42,8 @@ export default function Map({ innerRef, children, isStatic, ...mapViewProps }) {
         doubleTileSize={Platform.select({ android: true, ios: false })}
         minimumZ={3}
         shouldReplaceMapContent={true}
+        tileCachePath={tileCacheDirectory}
+        tileCacheMaxAge={60 * 60 * 24 * 7}
         // ios doubles the tile sizes despite setting doubleTileSize to false
         // this setting is to help resize them so that the labels aren't tiny
         tileSize={Platform.select({ android: 256, ios: 384 })}
