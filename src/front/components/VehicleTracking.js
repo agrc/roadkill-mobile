e5457fb89313ec64d062c47fe347fef552f45673
stdavis <stdavis@utah.gov>
2022-04-04 16:13:01 -0600

    chore(front): clean up annoying log

--- src/front/components/VehicleTracking.js
+++ src/front/components/VehicleTracking.js
@@ -57,8 +57,6 @@ export const initialVehicleTrackingState = {
 };
 
 export const vehicleTrackingReducer = (draft, action) => {
-  console.log('vehicleTrackingReducer', action.type);
-
   switch (action.type) {
     case 'SHOW':
       draft.isModalVisible = true;
