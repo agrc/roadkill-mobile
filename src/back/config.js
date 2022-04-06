a094ca04c3b3bbbf88d3b8735cfae03c06360575
stdavis <stdavis@utah.gov>
2022-04-06 14:26:00 -0600

    feat(back): finish implementing registration notification system
    Closes #82

--- src/back/config.js
+++ src/back/config.js
@@ -1,4 +1,5 @@
 export default {
   GCP_BUCKET_PHOTOS: `gs://${process.env.PROJECT_ID || process.env.ENVIRONMENT}-photos`,
   GCP_BUCKET_ID_IMAGES: `gs://${process.env.PROJECT_ID || process.env.ENVIRONMENT}-identification-images`,
+  APPROVAL_EXPIRATION_PERIOD: 1000 * 60 * 60 * 24 * 14, // two weeks
 };
