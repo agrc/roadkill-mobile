99aa537cecdff90f3ea48da234003d042411e349
stdavis <stdavis@utah.gov>
2022-09-16 12:12:28 -0600

    test(front): move function to easier module to test

--- src/front/services/utilities.test.js
+++ src/front/services/utilities.test.js
@@ -2,6 +2,7 @@ import {
   coordinatesToRegion,
   extentStringToRegion,
   getReleaseChannelBranch,
+  getSubmitValues,
   isTokenExpired,
   lineCoordinatesToString,
   lineStringToCoordinates,
@@ -190,5 +191,38 @@ describe('coordinatesToRegion', () => {
     expect(result.longitude).toBe(-112);
     expect(result.latitudeDelta).toBe(5.5);
     expect(result.longitudeDelta).toBe(4);
+  });
+});
+
+describe('getSubmitValues', () => {
+  it('copies values from input object', () => {
+    const input = { a: 1, b: 2 };
+
+    const output = getSubmitValues(input);
+
+    expect(output.a).toBe(1);
+    expect(output.b).toBe(2);
+  });
+  it('formats date objects', () => {
+    const input = { a: 1, b: new Date(1641320248681) };
+
+    const output = getSubmitValues(input);
+
+    expect(output.b).toEqual('2022-01-04T18:17:28.681Z');
+  });
+  it('handles objects as values', () => {
+    const input = { a: 1, b: { c: 2 } };
+
+    const output = getSubmitValues(input);
+
+    expect(output.b.c).toBe(2);
+  });
+  it('skips null values', () => {
+    const input = { a: 1, b: null, c: false };
+
+    const output = getSubmitValues(input);
+
+    expect(output.b).toBeUndefined();
+    expect(output.c).toBe(false);
   });
 });
