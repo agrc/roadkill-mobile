3eb33f62b0fd383e904dad6007af1493ecf2f320
stdavis <stdavis@utah.gov>
2022-10-24 13:09:45 -0600

    chore(website): bump deps including remix upgrade changes

--- src/website/app/root.jsx
+++ src/website/app/root.jsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import styles from './tailwind.css';
 
 export function meta() {
