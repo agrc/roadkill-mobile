eabf34f0d5994f4b224467aad1793c4ca8d7c8fa
stdavis <stdavis@utah.gov>
2022-10-25 11:26:20 -0600

    chore(front): upgrade for eas update

--- src/front/scripts/deployOTAUpdate.sh
+++ src/front/scripts/deployOTAUpdate.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-RELEASE_CHANNEL=$(./scripts/getReleaseChannel.sh)
-echo "Publishing new app builds for release channel: $RELEASE_CHANNEL"
-
 RELEASE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Publishing new app builds for release branch: $RELEASE_BRANCH"
+
 ENV_FILE="./.env.$RELEASE_BRANCH"
 echo "getting environment variables from $ENV_FILE"
 set -o allexport
 source $ENV_FILE
 set +o allexport
 
-./scripts/removeArtifacts.sh
-
-expo publish --release-channel $RELEASE_CHANNEL
+eas update --branch $RELEASE_BRANCH
