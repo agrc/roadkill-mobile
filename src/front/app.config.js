9f3c4410a5821d59dcd3a937e3ca5a7a03bca5e0
stdavis <stdavis@utah.gov>
2022-11-17 09:50:39 -0700

    release: v3.0.1 (578)

--- src/front/app.config.js
+++ src/front/app.config.js
@@ -16,7 +16,7 @@ const names = {
 const name = names[process.env.ENVIRONMENT];
 
 // perhaps this bump could be automated using a combo of app.config.json and this file?
-const buildNumber = 576;
+const buildNumber = 578;
 
 export default {
   name,
