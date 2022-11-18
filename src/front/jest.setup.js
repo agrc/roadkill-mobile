e77e0af8e5ec24d1a116702e1f8c83a789113414
stdavis <stdavis@utah.gov>
2022-11-18 12:01:04 -0700

    fix(front): mock constants in tests

--- src/front/jest.setup.js
+++ src/front/jest.setup.js
@@ -2,3 +2,12 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('expo-constants', () => {
+  return {
+    expoConfig: {
+      extra: {
+        API: '',
+      },
+    },
+  };
+});
