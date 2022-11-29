d5e551ab8435a174f8d5d841fe654f8fbeedbb26
stdavis <stdavis@utah.gov>
2022-11-29 16:16:39 -0700

    chore(front): standardize status bar components

--- src/front/components/FocusAwareStatusBar.js
+++ src/front/components/FocusAwareStatusBar.js
@@ -1,5 +1,5 @@
 import { useIsFocused } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 // cribbed from: https://reactnavigation.org/docs/status-bar/#tabs-and-drawer
 
