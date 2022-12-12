a61ca80a2984e8199137d94a556460c723f6ef5f
stdavis <stdavis@utah.gov>
2022-12-12 16:51:31 -0700

    fix(back): use more explicit table alias
    Hoping that this helps with this rare error:
    ```
    Error: Undefined binding(s) detected when compiling SELECT. Undefined column(s): [users.id] query: select "r"."report_id", "r"."photo_id", "r"."submit_date", "r"."common_name" from "report_infos" as "r" inner join "users" on "users"."id" = "r"."user_id" left join "pickup_reports" as "pick" on "pick"."report_id" = "r"."report_id" where "users"."id" = ? and "pick"."route_id" is null order by "r"."submit_date" desc limit ?
    ```

--- src/back/services/submissions.js
+++ src/back/services/submissions.js
@@ -3,23 +3,23 @@ import { db } from './clients.js';
 export async function getMySubmissions(user_id) {
   console.log('user_id', user_id);
   const reports = await db('report_infos as r')
-    .join('users', 'users.id', 'r.user_id')
+    .join('users as u', 'u.id', 'r.user_id')
     .leftJoin('pickup_reports as pick', 'pick.report_id', 'r.report_id')
     .select('r.report_id', 'r.photo_id', 'r.submit_date', 'r.common_name')
     .orderBy('r.submit_date', 'desc')
     .limit(100)
     .where({
-      'users.id': user_id,
+      'u.id': user_id,
       'pick.route_id': null, // don't show pickups that are tied to a route
     });
 
   const routes = await db('routes as r')
-    .join('users', 'users.id', 'r.user_id')
+    .join('users as u', 'u.id', 'r.user_id')
     .select('r.route_id', 'r.start_time', 'r.end_time', 'r.submit_date')
     .orderBy('r.submit_date', 'desc')
     .limit(100)
     .where({
-      'users.id': user_id,
+      'u.id': user_id,
     });
 
   return reports.concat(routes).sort((a, b) => b.submit_date - a.submit_date);
