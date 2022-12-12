47bc18017ecf9fc89b17c0c29a588167c98e6e1d
stdavis <stdavis@utah.gov>
2022-12-12 11:13:37 -0700

    build(front): upload sentry source maps

--- src/front/scripts/deployOTAUpdate.sh
+++ src/front/scripts/deployOTAUpdate.sh
@@ -10,6 +10,43 @@ echo "getting environment variables from $ENV_FILE"
 
 rm -rf ./dist
 
-npx env-cmd -f $ENV_FILE eas update --auto
+echo "publishing OTA update"
+results=$(npx env-cmd -f $ENV_FILE eas update --auto --json --non-interactive)
+
+echo "renaming files for sentry sourcemap upload"
+mv dist/bundles/android-7d6c4c5b200ae7b69ccec0a25f3285a4.js dist/bundles/index.android.bundle.js
+mv dist/bundles/ios-4b354f6df3dd2ef77c402a0aac9f2ecc.js dist/bundles/main.jsbundle.js
+
+config=$(eas config -p android -e $RELEASE_BRANCH | sed -n '/^{/,/^}/p' | jq -s '.[0]')
+
+bundle_id=$(echo $config | jq -r '.android .package')
+version=$(echo $config | jq -r '.version')
+build=$(echo $config | jq -r '.ios .buildNumber')
+
+if [[ $(echo $results | jq -r '.[0] .platform') == 'android' ]]; then
+  android_results=$(echo $results | jq '.[0]')
+  ios_results=$(echo $results | jq '.[1]')
+else
+  ios_results=$(echo $results | jq '.[0]')
+  android_results=$(echo $results | jq '.[1]')
+fi
+
+echo "uploading android sourcemaps to sentry"
+npx sentry-cli releases \
+  files $bundle_id@$version+$build \
+  upload-sourcemaps \
+  --dist $(echo $android_results | jq -r '.id') \
+  --rewrite dist/bundles/index.android.bundle.js $(find ./dist/bundles/android-*.map) \
+  --org utah-ugrc \
+  --project roadkill
+
+echo "uploading ios sourcemaps to sentry"
+npx sentry-cli releases \
+  files $bundle_id@$version+$build \
+  upload-sourcemaps \
+  --dist $(echo $ios_results | jq -r '.id') \
+  --rewrite dist/bundles/main.jsbundle.js $(find ./dist/bundles/ios-*.map) \
+  --org utah-ugrc \
+  --project roadkill
 
 say "over-the-air update published successfully"
