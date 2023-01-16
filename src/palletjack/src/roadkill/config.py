b399e986a4ab87ecc73809b478678b5adae436cc
stdavis <stdavis@utah.gov>
2023-01-16 09:18:24 -0700

    feat(palletjack): add project id to summary email

--- src/palletjack/src/roadkill/config.py
+++ src/palletjack/src/roadkill/config.py
@@ -3,14 +3,14 @@ config.py: Configuration values. Secrets to be handled with Secrets Manager
 """
 
 import logging
-import socket
+import os
 
 SKID_NAME = 'roadkill'
 
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
     'from_address': 'noreply@utah.gov',
     'to_addresses': 'stdavis@utah.gov',
-    'prefix': f'{SKID_NAME} on {socket.gethostname()}: ',
+    'prefix': f'{SKID_NAME} on {os.getenv("PROJECT_ID")}: ',
 }
 LOG_LEVEL = logging.DEBUG
 LOG_FILE_NAME = 'log'
