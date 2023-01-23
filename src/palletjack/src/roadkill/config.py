170a21575804ac2398674bd3609d8eca12040a5b
stdavis <stdavis@utah.gov>
2023-01-23 09:18:52 -0700

    wip(palletjack): try change detection
    This doesn't really work with polyline geometry but wanted to commit it in case I ever want to go back.

--- src/palletjack/src/roadkill/config.py
+++ src/palletjack/src/roadkill/config.py
@@ -5,19 +5,19 @@ config.py: Configuration values. Secrets to be handled with Secrets Manager
 import logging
 import os
 
-SKID_NAME = 'roadkill'
+SKID_NAME = "roadkill"
 
 SENDGRID_SETTINGS = {  #: Settings for SendGridHandler
-    'from_address': 'noreply@utah.gov',
-    'to_addresses': 'stdavis@utah.gov',
-    'prefix': f'{os.getenv("PROJECT_ID")}: ',
+    "from_address": "noreply@utah.gov",
+    "to_addresses": "stdavis@utah.gov",
+    "prefix": f'{os.getenv("PROJECT_ID")}: ',
 }
 LOG_LEVEL = logging.DEBUG
-LOG_FILE_NAME = 'log'
+LOG_FILE_NAME = "log"
 
 TABLES = [
-    #: ('table_name', 'id_column', 'geog_column', layer_index)
-    ('agol_pickup_reports', 'report_id', 'animal_location', 0),
-    ('agol_public_reports', 'report_id', 'animal_location', 1),
-    ('agol_routes', 'route_id', 'geog', 2),
+    #: ('table_name', 'id_column', 'geog_column')
+    ("agol_pickup_reports", "report_id", "animal_location"),
+    ("agol_public_reports", "report_id", "animal_location"),
+    ("agol_routes", "route_id", "geog"),
 ]
