9666ca690c4434b0c54478ab87bd2878d300753f
stdavis <stdavis@utah.gov>
2023-03-16 13:01:21 -0600

    feat(website): add privacy policy page
    #247

---
+++ src/website/app/routes/privacy-policy/index.jsx
@@ -0,0 +1,24 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl py-2">Privacy, User Account and Data Deletion</h1>
+      <p className="my-2">
+        The Utah Roadkill Reporter adheres to the{' '}
+        <a className="text-blue-500 underline" href="https://www.utah.gov/support/privacypolicy.html">
+          utah.gov Privacy Policy
+        </a>
+        .
+      </p>
+      <p className="my-2">
+        If you choose to{' '}
+        <a className="text-blue-500 underline" href="./delete-account">
+          delete your Utah Roadkill Reporter account
+        </a>
+        , all personally identifiable information (name, email, phone number) included in your account will be deleted,
+        while any reported animal location and species information you submitted will be kept for important
+        wildlife-related data analysis and decision making, and maintained pursuant to GRAMA and State of Utah record
+        retention requirements.
+      </p>
+    </div>
+  );
+}
