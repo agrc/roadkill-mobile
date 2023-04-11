2f41c0afa318bacf4ea638c15b186f87b4fef64b
stdavis <stdavis@utah.gov>
2023-04-11 12:05:14 -0600

    fix(website): add email request to privacy policy page
    #247

--- src/website/app/routes/privacy-policy/index.jsx
+++ src/website/app/routes/privacy-policy/index.jsx
@@ -17,7 +17,12 @@ export default function PrivacyPolicy() {
         , all personally identifiable information (name, email, phone number) included in your account will be deleted,
         while any reported animal location and species information you submitted will be kept for important
         wildlife-related data analysis and decision making, and maintained pursuant to GRAMA and State of Utah record
-        retention requirements.
+        retention requirements. To delete all personally identifiable information and any data you submitted using the
+        Roadkill Reporter app, please send an email request to{' '}
+        <a className="text-blue-500 underline" href="mailto:utah-roadkill-reporter-notify@utah.gov">
+          utah-roadkill-reporter-notify@utah.gov
+        </a>
+        .
       </p>
     </div>
   );
