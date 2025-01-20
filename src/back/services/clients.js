a1faa33256ac4c3f2ef5cabdc89b02a72758ee72
stdavis <stdavis@utah.gov>
2025-01-20 12:14:07 -0700

    fix(back): implement cloud sql connector

--- src/back/services/clients.js
+++ src/back/services/clients.js
@@ -1,3 +1,8 @@
+import {
+  AuthTypes,
+  Connector,
+  IpAddressTypes,
+} from '@google-cloud/cloud-sql-connector';
 import { Firestore } from '@google-cloud/firestore';
 import { Storage } from '@google-cloud/storage';
 import sgMail from '@sendgrid/mail';
@@ -6,10 +11,23 @@ import getSecret from './secrets.js';
 
 sgMail.setApiKey(getSecret('sendgrid-api-key'));
 
+const isTestOrLocalDev =
+  process.env.ENVIRONMENT === 'test' ||
+  process.env.ENVIRONMENT === 'development';
+const clientOpts = isTestOrLocalDev
+  ? {
+      host: '127.0.0.1',
+    }
+  : await new Connector().getOptions({
+      instanceConnectionName: process.env.CLOUDSQL_INSTANCE,
+      ipType: IpAddressTypes.PUBLIC,
+      authType: AuthTypes.PASSWORD,
+    });
+console.log('clientOpts', JSON.stringify(clientOpts, null, 2));
 export const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_HOST,
+    ...clientOpts,
     user: 'api',
     password: getSecret('database-password'),
     database: 'app',
