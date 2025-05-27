7e7e428584d14dcc017794c182735d10aaeacdc9
stdavis <stdavis@utah.gov>
2025-05-27 17:25:05 -0600

    release: v3.0.14 (650)

--- src/front/app.config.js
+++ src/front/app.config.js
@@ -12,7 +12,7 @@ const names = {
 const name = names[process.env.ENVIRONMENT];
 
 // perhaps this bump could be automated using a combo of app.config.json and this file?
-const buildNumber = 649;
+const buildNumber = 650;
 
 module.exports = {
   name,
@@ -22,7 +22,7 @@ module.exports = {
   owner: 'ugrc',
   scheme: bundleId,
   githubUrl: 'https://github.com/agrc/roadkill-mobile',
-  version: '3.0.13',
+  version: '3.0.14',
   orientation: 'portrait',
   icon:
     process.env.ENVIRONMENT === 'production'
@@ -139,7 +139,7 @@ module.exports = {
   /* required for eas update command
     bump major version when upgrading expo version
   */
-  runtimeVersion: '4.0.0',
+  runtimeVersion: '5.0.0',
   updates: {
     url: 'https://u.expo.dev/648c99de-696c-4704-8723-7f8838dc6896',
   },
